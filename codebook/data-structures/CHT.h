974c937a4b5b6cfffdad0fc2f2a83fa541cc2eb3
fffelix-huang <felixhuang07@gmail.com>
2023-02-11 22:12:06 +0800

    Add template for max/min

--- codebook/data-structures/CHT.h
+++ codebook/data-structures/CHT.h
@@ -5,10 +5,10 @@ struct Line_t {
 	inline bool operator<(long long x) const { return p < x; }
 };
 
-// returns maximum (with minimum use negative coefficient and constant)
+template<bool MAX = true>
 struct CHT : multiset<Line_t, less<>> {
 	// (for doubles, use INF = 1/.0, div(a,b) = a/b)
-	static const long long INF = LLONG_MAX;
+	static const long long INF = numeric_limits<long long>::max();
 	long long div(long long a, long long b) { // floored division
 		return a / b - ((a ^ b) < 0 && a % b);
 	}
@@ -27,6 +27,10 @@ struct CHT : multiset<Line_t, less<>> {
 	}
 
 	void add_line(long long k, long long m) {
+		if(!MAX) {
+			k = -k;
+			m = -m;
+		}
 		auto z = insert({k, m, 0}), y = z++, x = y;
 		while(isect(y, z)) {
 			z = erase(z);
@@ -42,6 +46,6 @@ struct CHT : multiset<Line_t, less<>> {
 	long long get(long long x) {
 		assert(!empty());
 		auto l = *lower_bound(x);
-		return l.k * x + l.m;
+		return (l.k * x + l.m) * (MAX ? +1 : -1);
 	}
 };
