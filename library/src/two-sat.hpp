945e326a483b8002a7910961d730a5931ca5212a
WeakMouse <felixhuang07@gmail.com>
2023-03-12 21:29:10 +0800

    Add more clauses

--- library/src/two-sat.hpp
+++ library/src/two-sat.hpp
@@ -11,9 +11,33 @@ public:
 	two_sat() : two_sat(0) {}
 	two_sat(int _n) : n(_n), g(_n * 2) {}
 
-	void add_clause(int u, bool x, int v, bool y) {
-		g.add_edge(2 * u + !x, 2 * v + y);
-		g.add_edge(2 * v + !y, 2 * u + x);
+	void add_implies_clause(int u, bool x, int v, bool y) {
+		g.add_edge(2 * u + x, 2 * v + y);
+		g.add_edge(2 * v + !y, 2 * u + !x);
+	}
+
+	void add_or_clause(int u, bool x, int v, bool y) {
+		add_implies_clause(u, !x, v, y);
+		add_implies_clause(v, !y, u, x);
+	}
+
+	void add_xor_clause(int u, bool x, int v, bool y) {
+		add_or_clause(u, x, v, y);
+		add_or_clause(u, !x, v, !y);
+	}
+
+	void add_equal_clause(int u, bool x, int v, bool y) {
+		add_implies_clause(u, x, v, y);
+		add_implies_clause(u, !x, v, !y);
+	}
+
+	void add_unequal_clause(int u, bool x, int v, bool y) {
+		add_implies_clause(u, x, v, !y);
+		add_implies_clause(u, !x, v, y);
+	}
+
+	void set_value(int u, bool x) {
+		add_or_clause(u, x, u, x);
 	}
 
 	bool satisfiable() {
