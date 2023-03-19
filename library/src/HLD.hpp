c2bc217ffe137c2854226b3b185d086238333c25
fffelix-huang <felixhuang07@gmail.com>
2023-03-20 01:03:57 +0800

    Add files

---
+++ library/src/HLD.hpp
@@ -0,0 +1,152 @@
+#ifndef FELIX_HLD_HPP
+#define FELIX_HLD_HPP 1
+
+#include "includes.hpp"
+#include "sparse-table.hpp"
+
+namespace felix {
+
+class HLD {
+private:
+	static constexpr std::pair<int, int> __lca_op(std::pair<int, int> a, std::pair<int, int> b) {
+		return min(a, b);
+	}
+
+public:
+	int n;
+	std::vector<std::vector<int>> g;
+	std::vector<int> subtree_size;
+	std::vector<int> parent;
+	std::vector<int> depth;
+	std::vector<int> top;
+	std::vector<int> tour;
+	std::vector<int> id;
+	std::vector<int> in, out;
+	sparse_table<std::pair<int, int>, __lca_op> st;
+
+	HLD(int _n) : n(_n), g(_n), subtree_size(_n), parent(_n), depth(_n), top(_n), id(_n), in(_n), out(_n) {
+		tour.reserve(n);
+	}
+
+	void add_edge(int u, int v) {
+		assert(0 <= u && u < n);
+		assert(0 <= v && v < n);
+		g[u].push_back(v);
+		g[v].push_back(u);
+	}
+
+	void build(int root = 0) {
+		assert(0 <= root && root < n);
+		parent[root] = -1;
+		top[root] = root;
+		std::vector<std::pair<int, int>> euler_tour;
+		euler_tour.reserve(2 * n - 1);
+		dfs_sz(root);
+		dfs_link(euler_tour, root);
+		st = sparse_table<std::pair<int, int>, __lca_op>(euler_tour);
+	}
+
+	int get_lca(int u, int v) {
+		assert(0 <= u && u < n);
+		assert(0 <= v && v < n);
+		int L = in[u];
+		int R = in[v];
+		if(L > R) {
+			std::swap(L, R);
+		}
+		return st.prod(L, R).second;
+	}
+
+	bool is_ancestor(int u, int v) {
+		assert(0 <= u && u < n);
+		assert(0 <= v && v < n);
+		return in[u] <= in[v] && in[v] < out[u];
+	}
+
+	bool on_path(int a, int b, int x) {
+		return (is_ancestor(x, a) || is_ancestor(x, b)) && is_ancestor(get_lca(a, b), x);
+	}
+
+	int get_distance(int u, int v) {
+		return depth[u] + depth[v] - 2 * depth[(get_lca(u, v))];
+	}
+
+	int get_kth_ancestor(int u, int k) {
+		assert(0 <= u && u < n);
+		if(depth[u] < k) {
+			return -1;
+		}
+		int d = depth[u] - k;
+		while(depth[top[u]] > d) {
+			u = parent[top[u]];
+		}
+		return tour[in[u] + d - depth[u]];
+	}
+
+	int get_kth_node_on_path(int a, int b, int k) {
+		int z = get_lca(a, b);
+		int fi = depth[a] - depth[z];
+		int se = depth[b] - depth[z];
+		assert(0 <= k && k <= fi + se);
+		if(k < fi) {
+			return get_kth_ancestor(a, k);
+		} else {
+			return get_kth_ancestor(b, fi + se - k);
+		}
+	}
+
+	std::vector<std::pair<int, int>> get_path(int u, int v, bool include_lca = true) {
+		if(u == v && !include_lca) {
+			return {};
+		}
+		std::vector<std::pair<int, int>> seg;
+		while(top[u] != top[v]) {
+			if(depth[top[u]] > depth[top[v]]) {
+				std::swap(u, v);
+			}
+			seg.emplace_back(id[top[v]], id[v]);
+			v = parent[top[v]];
+		}
+		if(depth[u] > depth[v]) {
+			std::swap(u, v);
+		}
+		if(u != v || include_lca) {
+			seg.emplace_back(id[u] + !include_lca, id[v]);
+		}
+		return seg;
+	}
+
+private:
+	void dfs_sz(int u) {
+		if(parent[u] != -1) {
+			g[u].erase(std::find(g[u].begin(), g[u].end(), parent[u]));
+		}
+		subtree_size[u] = 1;
+		for(auto& v : g[u]) {
+			parent[v] = u;
+			depth[v] = depth[u] + 1;
+			dfs_sz(v);
+			subtree_size[u] += subtree_size[v];
+			if(subtree_size[v] > subtree_size[g[u][0]]) {
+				std::swap(v, g[u][0]);
+			}
+		}
+	}
+
+	void dfs_link(std::vector<std::pair<int, int>>& euler_tour, int u) {
+		in[u] = (int) euler_tour.size();
+		id[u] = (int) tour.size();
+		euler_tour.emplace_back(depth[u], u);
+		tour.push_back(u);
+		for(auto v : g[u]) {
+			top[v] = (v == g[u][0] ? top[u] : v);
+			dfs_link(euler_tour, v);
+			euler_tour.emplace_back(depth[u], u);
+		}
+		out[u] = (int) euler_tour.size();
+	}
+};
+
+} // namespace felix
+
+#endif // FELIX_HLD_HPP
