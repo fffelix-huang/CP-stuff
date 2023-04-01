49d4b65ffbf7e35334d93a79d605ae120716335e
fffelix-huang <felixhuang07@gmail.com>
2023-04-01 22:02:06 +0800

    Add function

--- codebook/geometry/Geometry.h
+++ codebook/geometry/Geometry.h
@@ -35,3 +35,30 @@ Point<T> intersection_at(Point<T> a, Point<T> b, Point<T> c, Point<T> d) {
 	T a124 = cross(b - a, d - a);
 	return (d * a123 - c * a124) / (a123 - a124);
 }
+
+template<class T>
+int point_in_convex_hull(const vector<Point<T>>& a, const Point<T>& p) {
+	// -1 ON
+	//  0 OUT
+	// +1 IN
+	int n = (int) a.size();
+	if(between(a[0], a[1], p) || between(a[0], a[n - 1], p)) {
+		return -1;
+	}
+	int l = 0, r = n - 1;
+	while(l <= r) {
+		int m = (l + r) / 2;
+		auto a1 = cross(a[m] - a[0], p - a[0]);
+		auto a2 = cross(a[(m + 1) % n] - a[0], p - a[0]);
+		if(a1 >= 0 && a2 <= 0) {
+			auto res = cross(a[(m + 1) % n] - a[m], p - a[m]);
+			return res > 0 ? 1 : (res >= 0 ? -1 : 0);
+		}
+		if(a1 < 0) {
+			r = m - 1;
+		} else {
+			l = m + 1;
+		}
+	}
+	return 0;
+}
