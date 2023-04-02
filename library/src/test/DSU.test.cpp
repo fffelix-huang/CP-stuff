eb52c380c1a4c0e8f7819d15d58e0d2758800652
fffelix-huang <felixhuang07@gmail.com>
2023-04-02 16:18:38 +0800

    Add test for DSU

---
+++ library/src/test/DSU.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+
+#include "DSU.hpp"
+using namespace std;
+using namespace felix;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n, q;
+	cin >> n >> q;
+	DSU d(n);
+	while(q--) {
+		int type, u, v;
+		cin >> type >> u >> v;
+		if(type == 0) {
+			d.merge(u, v);
+		} else {
+			cout << d.same(u, v) << "\n";
+		}
+	}
+	return 0;
+}
