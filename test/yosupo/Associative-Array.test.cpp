a601175b7237ae9f51b038cee055fbc1470c4b3d
fffelix-huang <felixhuang07@gmail.com>
2023-04-03 23:43:13 +0800

    Add test

---
+++ test/yosupo/Associative-Array.test.cpp
@@ -0,0 +1,27 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/associative_array"
+
+#include <iostream>
+#include "../../library/data-structure/PBDS.hpp"
+using namespace std;
+using namespace felix;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	hash_map<long long, long long> mp;
+	int q;
+	cin >> q;
+	while(q--) {
+		int type;
+		long long k;
+		cin >> type >> k;
+		if(type == 0) {
+			long long x;
+			cin >> x;
+			mp[k] = x;
+		} else {
+			cout << mp[k] << "\n";
+		}
+	}
+	return 0;
+}
