a2f451d3a0182f47beb8b71d1f47c3abe67e68c9
fffelix-huang <felixhuang07@gmail.com>
2023-04-07 17:24:34 +0800

    Update test

--- test/yosupo/Convolution-Mod-1000000007.test.cpp
+++ test/yosupo/Convolution-Mod-1000000007.test.cpp
@@ -2,42 +2,30 @@
 
 #include <iostream>
 #include "../../library/math/crt.hpp"
-#include "../../library/modint/modint.hpp"
-#include "../../library/formal-power-series/poly.hpp"
+#include "../../library/convolution/NTT.hpp"
 using namespace std;
 using namespace felix;
 
-using m0 = modint998244353;
-using m1 = modint<754974721>;
-using m2 = modint<469762049>;
+constexpr int NTT_PRIMES[] = {469762049, 754974721, 998244353};
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	int n, m;
 	cin >> n >> m;
-	Poly<m0> a0(n), b0(m);
-	Poly<m1> a1(n), b1(m);
-	Poly<m2> a2(n), b2(m);
+	vector<int> a(n);
 	for(int i = 0; i < n; i++) {
-		int x;
-		cin >> x;
-		a0[i] = x;
-		a1[i] = x;
-		a2[i] = x;
+		cin >> a[i];
 	}
+	vector<int> b(m);
 	for(int i = 0; i < m; i++) {
-		int x;
-		cin >> x;
-		b0[i] = x;
-		b1[i] = x;
-		b2[i] = x;
+		cin >> b[i];
 	}
-	auto c0 = a0 * b0;
-	auto c1 = a1 * b1;
-	auto c2 = a2 * b2;
+	auto c0 = NTT<NTT_PRIMES[0]>::multiply(a, b);
+	auto c1 = NTT<NTT_PRIMES[1]>::multiply(a, b);
+	auto c2 = NTT<NTT_PRIMES[2]>::multiply(a, b);
 	for(int i = 0; i < n + m - 1; i++) {
-		cout << (int) (crt(vector<__int128>{c0[i](), c1[i](), c2[i]()}, vector<__int128>{m0::mod(), m1::mod(), m2::mod()}).first % ((int) 1e9 + 7)) << " \n"[i == n + m - 2];
+		cout << (int) (crt(vector<__int128>{c0[i], c1[i], c2[i]}, vector<__int128>{NTT_PRIMES, NTT_PRIMES + 3}).first % ((int) 1e9 + 7)) << " \n"[i == n + m - 2];
 	}
 	return 0;
 }
