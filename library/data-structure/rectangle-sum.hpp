b60ca6d8bbcabd081d11c795c07010804470dd52
WeakMouse <felixhuang07@gmail.com>
2023-04-10 16:57:50 +0800

    Add rectangle-sum and test

---
+++ library/data-structure/rectangle-sum.hpp
@@ -0,0 +1,85 @@
+#pragma once
+#include <vector>
+#include <algorithm>
+#include "fenwick.hpp"
+
+namespace felix {
+
+template<class T, class Weight_t>
+struct RectangleSum {
+	struct Query {
+		T x, y;
+		Weight_t w;
+		int id;
+
+		Query() {}
+		Query(T _x, T _y, Weight_t _w, int _id) : x(_x), y(_y), w(_w), id(_id) {}
+	};
+
+	void add_point(T x, T y, Weight_t w) {
+		queries.emplace_back(x, y, w, -1);
+	}
+
+	void add_query(T x, T y, T x2, T y2) {
+		queries.emplace_back(x, y, +1, qid);
+		queries.emplace_back(x, y2, -1, qid);
+		queries.emplace_back(x2, y, -1, qid);
+		queries.emplace_back(x2, y2, +1, qid);
+		qid += 1;
+	}
+
+	std::vector<Weight_t> solve() {
+		std::vector<T> ys;
+		for(auto& q : queries) {
+			ys.push_back(q.y);
+		}
+		std::sort(ys.begin(), ys.end());
+		ys.erase(std::unique(ys.begin(), ys.end()), ys.end());
+		sz = (int) ys.size();
+		for(auto& q : queries) {
+			q.y = std::lower_bound(ys.begin(), ys.end(), q.y) - ys.begin();
+		}
+		ans.assign(qid, 0);
+		fenw = fenwick<Weight_t>(sz);
+		solve(0, queries.size());
+		return ans;
+	}
+
+private:
+	int qid = 0, sz;
+	std::vector<Query> queries;
+	std::vector<Weight_t> ans;
+	fenwick<Weight_t> fenw;
+
+	void solve(int l, int r) {
+		if(l + 1 == r) {
+			return;
+		}
+		int mid = (l + r) / 2;
+		solve(l, mid);
+		solve(mid, r);
+		int i = l;
+		for(int j = mid; j < r; j++) {
+			const Query& q = queries[j];
+			while(i < mid && queries[i].x >= q.x) {
+				if(queries[i].id == -1) {
+					fenw.add(queries[i].y, queries[i].w);
+				}
+				i += 1;
+			}
+			if(q.id >= 0) {
+				ans[q.id] += q.w * fenw.sum(q.y, sz - 1);
+			}
+		}
+		for(int p = l; p < i; p++) {
+			if(queries[p].id == -1) {
+				fenw.add(queries[p].y, -queries[p].w);
+			}
+		}
+		std::inplace_merge(queries.begin() + l, queries.begin() + mid, queries.begin() + r, [](const Query& a, const Query& b) {
+			return a.x > b.x;
+		});
+	}
+};
+
+} // namespace felix
---
+++ test/yosupo/Point-Add-Rectangle-Sum.test.cpp
@@ -0,0 +1,38 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_add_rectangle_sum"
+
+#include <iostream>
+#include "data-structure/rectangle-sum.hpp"
+using namespace std;
+using namespace felix;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n, q;
+	cin >> n >> q;
+	RectangleSum<int, long long> solver;
+	for(int i = 0; i < n; i++) {
+		int x, y;
+		long long w;
+		cin >> x >> y >> w;
+		solver.add_point(x, y, w);
+	}
+	while(q--) {
+		int type, x, y;
+		cin >> type >> x >> y;
+		if(type == 0) {
+			long long w;
+			cin >> w;
+			solver.add_point(x, y, w);
+		} else {
+			int x2, y2;
+			cin >> x2 >> y2;
+			solver.add_query(x, y, x2, y2);
+		}
+	}
+	auto ans = solver.solve();
+	for(auto x : ans) {
+		cout << x << "\n";
+	}
+	return 0;
+}
