350c5d92651b089377dbc8cf7b56efd1050e577a
fffelix-huang <felixhuang07@gmail.com>
2023-04-16 03:12:19 +0800

    Use barrett

--- library/math/discrete-log.hpp
+++ library/math/discrete-log.hpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <cassert>
 #include "../data-structure/PBDS.hpp"
+#include "../modint/barrett.hpp"
 #include "binary-gcd.hpp"
 
 namespace felix {
@@ -14,15 +15,16 @@ int discrete_log(int a, int b, int m) {
 	}
 	int n = (int) std::sqrt(m) + 1, e = 1, f = 1, j = 1;
 	hash_map<int, int> baby;
-	while(j <= n && (e = f = 1LL * e * a % m) != b) {
-		baby[1LL * e * b % m] = j++;
+	internal::barrett bt(m);
+	while(j <= n && (e = f = bt.mul(e, a)) != b) {
+		baby[bt.mul(e, b)] = j++;
 	}
 	if(e == b) {
 		return j;
 	}
 	if(binary_gcd(m, e) == binary_gcd(m, b))  {
 		for(int i = 2; i < n + 2; i++) {
-			e = 1LL * e * f % m;
+			e = bt.mul(e, f);
 			if(baby.find(e) != baby.end()) {
 				return n * i - baby[e];
 			}
