4839da625ede0104123d1017694e71f2a196a6e1
WeakMouse <felixhuang07@gmail.com>
2023-04-15 17:36:20 +0800

    Rewrite wavelet-tree to handle different types

--- docs/wavelet-tree.md
+++ docs/wavelet-tree.md
@@ -5,10 +5,17 @@ documentation_of: ../library/data-structure/wavelet-tree.hpp
 
 ## 使用方法
 ```cpp
-vector<int> v;
-wavelet_tree tree(v); // 初始化
+vector<int> v = {4, 8, 7, 6, 3};
+vector<pair<int, int>> v2({{3, 1}, {4, 1}, {5, 9}, {2, 6}});
+
+// 初始化
+wavelet_tree tree(v);
+wavelet_tree tree2(v2);
 
 int l, r, k;
-int val = tree.kth(l, r, k); // 回傳區間 [l, r) 的第 k 大 (0-based)
+int val = tree.get_kth(l, r, k); // 回傳區間 [l, r) 的第 k 大 (0-based)
+int x = tree.get_kth(0, 3, 0); // 4
+int y = tree.get_kth(1, 3, 1); // 8
+pair<int, int> res = tree2.get_kth(1, 4, 1); // {4, 1};
 ```
 
--- library/data-structure/wavelet-tree.hpp
+++ library/data-structure/wavelet-tree.hpp
@@ -4,32 +4,33 @@
 
 namespace felix {
 
+template<class T>
 class wavelet_tree {
 public:
 	wavelet_tree() {}
-	explicit wavelet_tree(std::vector<int> v) : vals(v), t(0), n(v.size()) {
+	explicit wavelet_tree(const std::vector<T>& _v) : vals(_v), n(_v.size()) {
 		std::sort(vals.begin(), vals.end());
 		vals.erase(std::unique(vals.begin(), vals.end()), vals.end());
-		t = std::__lg(2 * vals.size() - 1);
-		bits.resize((t * n + 64) >> 6, 0ULL);
+		log = std::__lg(2 * vals.size() - 1);
+		bits.resize((log * n + 64) >> 6, 0ULL);
 		sums.resize(bits.size(), 0);
-		std::vector<int> cnt(vals.size() + 1);
-		for(int& x : v) {
-			x = std::lower_bound(vals.begin(), vals.end(), x) - vals.begin();
-			cnt[x + 1] += 1;
+		std::vector<int> v(_v.size()), cnt(vals.size() + 1);
+		for(int i = 0; i < (int) v.size(); i++) {
+			v[i] = std::lower_bound(vals.begin(), vals.end(), _v[i]) - vals.begin();
+			cnt[v[i] + 1] += 1;
 		}
 		for(int i = 1; i < (int) vals.size(); ++i) {
 			cnt[i] += cnt[i - 1];
 		}
-		for(int j = 0; j < t; ++j) {
+		for(int j = 0; j < log; ++j) {
 			for(int i : v) {
-				int tmp = i >> (t - 1 - j);
-				int pos = (tmp >> 1) << (t - j);
-				setBit(j * n + cnt[pos], tmp & 1);
+				int tmp = i >> (log - 1 - j);
+				int pos = (tmp >> 1) << (log - j);
+				set_bit(j * n + cnt[pos], tmp & 1);
 				cnt[pos] += 1;
 			}
 			for(int i : v) {
-				cnt[(i >> (t - j)) << (t - j)] -= 1;
+				cnt[(i >> (log - j)) << (log - j)] -= 1;
 			}
 		}
 		for(int i = 1; i < (int) sums.size(); ++i) {
@@ -37,19 +38,18 @@ public:
 		}
 	}
 
-	// Returns kth (0-based) smallest element in range [a, b)
-	int kth(int a, int b, int k) {
-		for(int j = 0, ia = 0, ib = n, res = 0;; ++j) {
-			if(j == t) {
+	T get_kth(int a, int b, int k) {
+		for(int j = 0, ia = 0, ib = n, res = 0;; j++) {
+			if(j == log) {
 				return vals[res];
 			}
-			int cnt_ia = getSum(n * j + ia);
-			int cnt_a = getSum(n * j + a);
-			int cnt_b = getSum(n * j + b);
-			int cnt_ib = getSum(n * j + ib);
+			int cnt_ia = get_sum(n * j + ia);
+			int cnt_a = get_sum(n * j + a);
+			int cnt_b = get_sum(n * j + b);
+			int cnt_ib = get_sum(n * j + ib);
 			int ab_zeros = (b - a) - (cnt_b - cnt_a);
 			if(ab_zeros > k) {
-				res = res << 1;
+				res <<= 1;
 				ib -= cnt_ib - cnt_ia;
 				a -= cnt_a - cnt_ia;
 				b -= cnt_b - cnt_ia;
@@ -64,15 +64,16 @@ public:
 	}
 
 private:
-	std::vector<int> vals, sums;
+	int n, log;
+	std::vector<T> vals;
+	std::vector<int> sums;
 	std::vector<unsigned long long> bits;
-	int t, n;
 
-	inline void setBit(int i, unsigned long long v) { 
+	inline void set_bit(int i, unsigned long long v) { 
 		bits[i >> 6] |= (v << (i & 63));
 	}
 
-	inline int getSum(int i) const {
+	inline int get_sum(int i) const {
 		return sums[i >> 6] + __builtin_popcountll(bits[i >> 6] & ((1ULL << (i & 63)) - 1));
 	}
 };
