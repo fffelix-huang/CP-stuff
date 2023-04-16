fce7c7b64408d9cda3c3aebba1739e729ec77d60
WeakMouse <felixhuang07@gmail.com>
2023-04-16 22:59:03 +0800

    Put naive and doubling into sa_is

--- library/string/suffix-array.hpp
+++ library/string/suffix-array.hpp
@@ -7,52 +7,8 @@
 
 namespace felix {
 
-std::vector<int> sa_naive(const std::vector<int>& s) {
-	int n = int(s.size());
-	std::vector<int> sa(n);
-	std::iota(sa.begin(), sa.end(), 0);
-	std::sort(sa.begin(), sa.end(), [&](int l, int r) {
-		if(l == r) {
-			return false;
-		}
-		while(l < n && r < n) {
-			if(s[l] != s[r]) {
-				return s[l] < s[r];
-			}
-			l++;
-			r++;
-		}
-		return l == n;
-	});
-	return sa;
-}
+namespace internal {
 
-std::vector<int> sa_doubling(const std::vector<int>& s) {
-	int n = int(s.size());
-	std::vector<int> sa(n), rnk = s, tmp(n);
-	std::iota(sa.begin(), sa.end(), 0);
-	for(int k = 1; k < n; k *= 2) {
-		auto cmp = [&](int x, int y) {
-			if(rnk[x] != rnk[y]) return rnk[x] < rnk[y];
-			int rx = x + k < n ? rnk[x + k] : -1;
-			int ry = y + k < n ? rnk[y + k] : -1;
-			return rx < ry;
-		};
-		std::sort(sa.begin(), sa.end(), cmp);
-		tmp[sa[0]] = 0;
-		for(int i = 1; i < n; i++) {
-			tmp[sa[i]] = tmp[sa[i - 1]] + (cmp(sa[i - 1], sa[i]) ? 1 : 0);
-		}
-		swap(tmp, rnk);
-	}
-	return sa;
-}
-
-// SA-IS, linear-time suffix array construction
-// Reference:
-// G. Nong, S. Zhang, and W. H. Chan,
-// Two Efficient Algorithms for Linear Time Suffix Array Construction
-template<int THRESHOLD_NAIVE = 10, int THRESHOLD_DOUBLING = 40>
 std::vector<int> sa_is(const std::vector<int>& s, int upper) {
 	int n = (int) s.size();
 	if(n == 0) {
@@ -68,24 +24,41 @@ std::vector<int> sa_is(const std::vector<int>& s, int upper) {
 			return {1, 0};
 		}
 	}
-	if(n < THRESHOLD_NAIVE) {
-		return sa_naive(s);
-	}
-	if(n < THRESHOLD_DOUBLING) {
-		return sa_doubling(s);
-	}
 	std::vector<int> sa(n);
+	if(n < 10) {
+		std::iota(sa.begin(), sa.end(), 0);
+		std::sort(sa.begin(), sa.end(), [&](int i, int j) {
+			return std::lexicographical_compare(s.begin() + i, s.end(), s.begin() + j, s.end());
+		});
+		return sa;
+	}
+	if(n < 40) {
+		std::vector<int> rnk = s, tmp(n);
+		std::iota(sa.begin(), sa.end(), 0);
+		for(int k = 1; k < n; k *= 2) {
+			auto cmp = [&](int x, int y) {
+				if(rnk[x] != rnk[y]) return rnk[x] < rnk[y];
+				int rx = x + k < n ? rnk[x + k] : -1;
+				int ry = y + k < n ? rnk[y + k] : -1;
+				return rx < ry;
+			};
+			std::sort(sa.begin(), sa.end(), cmp);
+			tmp[sa[0]] = 0;
+			for(int i = 1; i < n; i++) {
+				tmp[sa[i]] = tmp[sa[i - 1]] + cmp(sa[i - 1], sa[i]);
+			}
+			std::swap(tmp, rnk);
+		}
+		return sa;
+	}
 	std::vector<bool> ls(n);
 	for(int i = n - 2; i >= 0; i--) {
 		ls[i] = (s[i] == s[i + 1]) ? ls[i + 1] : (s[i] < s[i + 1]);
 	}
 	std::vector<int> sum_l(upper + 1), sum_s(upper + 1);
 	for(int i = 0; i < n; i++) {
-		if(!ls[i]) {
-			sum_s[s[i]]++;
-		} else {
-			sum_l[s[i] + 1]++;
-		}
+		sum_s[s[i]] += !ls[i];
+		sum_l[s[i] + 1] += ls[i];
 	}
 	for(int i = 0; i <= upper; i++) {
 		sum_s[i] += sum_l[i];
@@ -93,10 +66,9 @@ std::vector<int> sa_is(const std::vector<int>& s, int upper) {
 			sum_l[i + 1] += sum_s[i];
 		}
 	}
-
+	std::vector<int> buf(upper + 1);
 	auto induce = [&](const std::vector<int>& lms) {
 		std::fill(sa.begin(), sa.end(), -1);
-		std::vector<int> buf(upper + 1);
 		std::copy(sum_s.begin(), sum_s.end(), buf.begin());
 		for(auto d : lms) {
 			if(d == n) {
@@ -120,7 +92,6 @@ std::vector<int> sa_is(const std::vector<int>& s, int upper) {
 			}
 		}
 	};
-
 	std::vector<int> lms_map(n + 1, -1);
 	int m = 0;
 	for(int i = 1; i < n; i++) {
@@ -135,9 +106,7 @@ std::vector<int> sa_is(const std::vector<int>& s, int upper) {
 			lms.push_back(i);
 		}
 	}
-
 	induce(lms);
-
 	if(m) {
 		std::vector<int> sorted_lms;
 		sorted_lms.reserve(m);
@@ -153,29 +122,10 @@ std::vector<int> sa_is(const std::vector<int>& s, int upper) {
 			int l = sorted_lms[i - 1], r = sorted_lms[i];
 			int end_l = (lms_map[l] + 1 < m) ? lms[lms_map[l] + 1] : n;
 			int end_r = (lms_map[r] + 1 < m) ? lms[lms_map[r] + 1] : n;
-			bool same = true;
-			if(end_l - l != end_r - r) {
-				same = false;
-			} else {
-				while(l < end_l) {
-					if(s[l] != s[r]) {
-						break;
-					}
-					l++;
-					r++;
-				}
-				if(l == n || s[l] != s[r]) {
-					same = false;
-				}
-			}
-			if(!same) {
-				rec_upper++;
-			}
-			rec_s[lms_map[sorted_lms[i]]] = rec_upper;
+			rec_upper += !std::equal(s.begin() + l, s.begin() + end_l, s.begin() + r, s.begin() + end_r);
+			rec_s[lms_map[r]] = rec_upper;
 		}
-
-		auto rec_sa = sa_is<THRESHOLD_NAIVE, THRESHOLD_DOUBLING>(rec_s, rec_upper);
-
+		auto rec_sa = sa_is(rec_s, rec_upper);
 		for(int i = 0; i < m; i++) {
 			sorted_lms[i] = lms[rec_sa[i]];
 		}
@@ -184,12 +134,14 @@ std::vector<int> sa_is(const std::vector<int>& s, int upper) {
 	return sa;
 }
 
+} // namespace internal
+
 std::vector<int> suffix_array(const std::vector<int>& s, int upper) {
 	assert(0 <= upper);
 	for(int d : s) {
 		assert(0 <= d && d <= upper);
 	}
-	auto sa = sa_is(s, upper);
+	auto sa = internal::sa_is(s, upper);
 	return sa;
 }
 
@@ -207,7 +159,7 @@ std::vector<int> suffix_array(const std::vector<T>& s) {
 		}
 		s2[idx[i]] = now;
 	}
-	return sa_is(s2, now);
+	return internal::sa_is(s2, now);
 }
 
 std::vector<int> suffix_array(const std::string& s) {
@@ -216,7 +168,7 @@ std::vector<int> suffix_array(const std::string& s) {
 	for(int i = 0; i < n; i++) {
 		s2[i] = s[i];
 	}
-	return sa_is(s2, 255);
+	return internal::sa_is(s2, 255);
 }
 
 } // namespace felix
