81b38a51c6f2052434a788e022a9bd54e963c5c9
fffelix-huang <felixhuang07@gmail.com>
2023-04-17 13:53:00 +0800

    Fixed error

--- test/yosupo/Convolution-Mod-1000000007.test.cpp
+++ test/yosupo/Convolution-Mod-1000000007.test.cpp
@@ -1,12 +1,12 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/convolution_mod_1000000007"
 
 #include <iostream>
-#include "../../library/math/crt.hpp"
+#include "../../library/modint/modint.hpp"
 #include "../../library/convolution/NTT.hpp"
 using namespace std;
 using namespace felix;
 
-constexpr int NTT_PRIMES[] = {469762049, 754974721, 998244353};
+using mint = modint1000000007;
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -21,11 +21,9 @@ int main() {
 	for(int i = 0; i < m; i++) {
 		cin >> b[i];
 	}
-	auto c0 = NTT<NTT_PRIMES[0]>::multiply(a, b);
-	auto c1 = NTT<NTT_PRIMES[1]>::multiply(a, b);
-	auto c2 = NTT<NTT_PRIMES[2]>::multiply(a, b);
+	auto c = convolution_u128(a, b);
 	for(int i = 0; i < n + m - 1; i++) {
-		cout << (int) (crt(vector<__int128>{c0[i], c1[i], c2[i]}, vector<__int128>{NTT_PRIMES, NTT_PRIMES + 3}).first % ((int) 1e9 + 7)) << " \n"[i == n + m - 2];
+		cout << mint(c[i]) << " \n"[i == n + m - 2];
 	}
 	return 0;
 }
