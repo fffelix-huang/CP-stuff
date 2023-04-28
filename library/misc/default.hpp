b8c192b7ecd7656046397a9c1d5cfc772f376c03
WeakMouse <felixhuang07@gmail.com>
2023-04-28 14:32:43 +0800

    Small modification

--- library/misc/default.hpp
+++ library/misc/default.hpp
@@ -1,9 +1,9 @@
 #pragma once
 #include <bits/stdc++.h>
 
-#define all(v) (v).begin(), (v).end()
-#define rall(v) (v).rbegin(), (v).rend()
-#define sz(v) ((int) (v).size())
+#define ALL(v) (v).begin(), (v).end()
+#define RALL(v) (v).rbegin(), (v).rend()
+#define SZ(v) ((int) (v).size())
 #define FOR(i, begin, end) for(int i = (begin), i##_end_ = (end); i < i##_end_; i++)
 #define IFOR(i, begin, end) for(int i = (end) - 1, i##_begin_ = (begin); i >= i##_begin_; i--)
 #define REP(i, n) FOR(i, 0, n)
@@ -23,20 +23,15 @@ template<class T> std::istream& operator>>(std::istream& in, std::vector<T>& a) 
 template<class Fun>
 class y_combinator_result {
 	Fun fun_;
+	
 public:
 	template<class T>
 	explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
 
-	template<class ...Args>
-	decltype(auto) operator()(Args &&...args) {
-		return fun_(std::ref(*this), std::forward<Args>(args)...);
-	}
+	template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
 };
 
-template<class Fun>
-decltype(auto) y_combinator(Fun &&fun) {
-	return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
-}
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
 
 template<class T> bool chmin(T& a, const T& b) { return a < b ? false : (a = b, true); }
 template<class T> bool chmax(T& a, const T& b) { return a > b ? false : (a = b, true); }
--- settings/cleaner.py
+++ settings/cleaner.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 
 	for line in s.splitlines():
 		T = line.strip()
-		if T.find('debug(') != -1 or T == '#include "misc/debug.hpp"':
+		if T.find('debug') != -1:
 			continue
 		result.append(line)
 
