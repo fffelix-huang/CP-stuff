6abddeca112340e7a0dc3d3054da9600689c9f9f
fffelix-huang <felixhuang07@gmail.com>
2023-05-06 20:48:33 +0800

    Fixed bug

--- library/misc/default.hpp
+++ library/misc/default.hpp
@@ -33,8 +33,8 @@ public:
 
 template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
 
-template<class T> bool chmin(T& a, const T& b) { return a < b ? false : (a = b, true); }
-template<class T> bool chmax(T& a, const T& b) { return a > b ? false : (a = b, true); }
+template<class T> bool chmin(T& a, const T& b) { return a > b ? (a = b, true) : false; }
+template<class T> bool chmax(T& a, const T& b) { return a < b ? (a = b, true) : false; }
 template<class T> std::vector<T> sort_unique(std::vector<T> v) { std::sort(v.begin(), v.end()), v.erase(std::unique(v.begin(), v.end()), v.end()); return v; }
 
 namespace felix {}
