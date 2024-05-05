4761ab430cf09398f7f829b586c49d0c303494cb
Ting-Hsuan Huang <felixhuang07@gmail.com>
2024-05-06 01:33:48 +0800

    Update install.sh and add README.md

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+if [[ $FELIX_CP_LIBRARY_INSTALLED == 1 ]]; then
+	echo "[INFO] Library already installed!"
+	exit 1
+fi
+
 current_shell=$SHELL
 
 case $current_shell in
@@ -7,20 +12,24 @@ case $current_shell in
 		echo "[INFO] Adding PATH to ~/.bashrc"
 		echo "export FELIX_CP_LIBRARY_PATH=\"$(pwd)\"" >> ~/.bashrc
 		echo "source \$FELIX_CP_LIBRARY_PATH/tools/cp-bash-commands.sh" >> ~/.bashrc
+		echo "export FELIX_CP_LIBRARY_INSTALLED=1" >> ~/.bashrc
 
-		echo "[INFO] Recompile ~/.zshrc"
+		echo "[INFO] Recompile ~/.bashrc"
 		source ~/.bashrc
 		;;
+
 	/bin/zsh)
 		echo "[INFO] Setting up for zsh environment..."
 
 		echo "[INFO] Adding PATH to ~/.zshrc"
 		echo "export FELIX_CP_LIBRARY_PATH=\"$(pwd)\"" >> ~/.zshrc
 		echo "source \$FELIX_CP_LIBRARY_PATH/tools/cp-zsh-commands.zsh" >> ~/.zshrc
+		echo "export FELIX_CP_LIBRARY_INSTALLED=1" >> ~/.zshrc
 
 		echo "[INFO] Recompile ~/.zshrc"
 		source ~/.zshrc
 		;;
+
 	*)
 		echo "[INFO] Shell not supported :("
 		exit
