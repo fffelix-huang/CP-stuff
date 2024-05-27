a81ec648c8e0215b9708d4b694285857030d6fdb
Ting-Hsuan Huang <felixhuang07@gmail.com>
2024-05-28 02:29:27 +0800

    Fixed bug in inversion.hpp

--- library/counting/inversion.hpp
+++ library/counting/inversion.hpp
@@ -6,11 +6,11 @@
 namespace felix {
 
 template<class T, class Func>
-long long count_inversions(std::vector<T> a, Func f = std::less<T>()) {
+long long count_inversions(std::vector<T> a, const Func& f = std::less<T>()) {
 	std::vector<T> buf(a.size());
 	std::function<long long(int, int)> solve = [&](int l, int r) {
 		if(l + 1 == r) {
-			return 0;
+			return 0LL;
 		}
 		int mid = (l + r) / 2;
 		long long ans = solve(l, mid) + solve(mid, r);
@@ -20,6 +20,7 @@ long long count_inversions(std::vector<T> a, Func f = std::less<T>()) {
 				buf[p++] = a[i++];
 			} else {
 				buf[p++] = a[j++];
+				ans += mid - i;
 			}
 		}
 		std::copy(buf.begin(), buf.begin() + p, a.begin() + l);
